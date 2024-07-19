6760880fc3bf8ed8e2afb69063a6be161487e4b8
Yifan Liu <yifanliu.liu@mail.utoronto.ca>
2024-07-19 17:34:39 -0400

    update

--- src/Retriever/sparseRetriever.py
+++ src/Retriever/sparseRetriever.py
@@ -1,6 +1,7 @@
 from src.Retriever.denseRetriever import *
 from rank_bm25 import BM25Okapi
 from sparsembed import model, retrieve
+from nltk.tokenize import word_tokenize
 import torch
 from transformers import AutoModelForMaskedLM, AutoTokenizer
 from src.Entity.aspect import Aspect
@@ -13,19 +14,19 @@ class SparseRetriever(AbstractRetriever):
     Concrete SparseRetriever class.
     """
     cls_type = "sparse"
-    def __init__(self, query_path: str, chunks_dir: str, output_path: str, num_chunks: int = 3):
-        super().__init__(query_path=query_path, output_path=output_path, chunks_dir=chunks_dir, num_chunks=num_chunks)
+    def __init__(self, query_path: str, chunks_dir: str, output_path: str, num_chunks: Optional[int] = None, percentile: Optional[float] = None, threshold: Optional[float] = None):
+        super().__init__(query_path=query_path, output_path=output_path, chunks_dir=chunks_dir, num_chunks=num_chunks, percentile=percentile, threshold=threshold)
 
 
 class BM25Retriever(SparseRetriever):
     """
     Concrete SparseRetriever class.
     """
-    def __init__(self, query_path: str, chunks_dir:str, output_path: str, num_chunks: int = 3):
-        super().__init__(query_path=query_path, output_path=output_path, chunks_dir=chunks_dir, num_chunks=num_chunks)
+    def __init__(self, query_path: str, chunks_dir:str, output_path: str, num_chunks: Optional[int] = None, percentile: Optional[float] = None, threshold: Optional[float] = None):
+        super().__init__(query_path=query_path, output_path=output_path, chunks_dir=chunks_dir, num_chunks=num_chunks, percentile=percentile, threshold=threshold)
     
 
-    def retrieval_for_dest(self, aspects: list[Aspect], dest_chunks: list[str], num_chunks: int = 3) -> dict[str, tuple[float, list[str]]]:
+    def retrieval_for_dest(self, aspects: list[Aspect], dest_chunks: list[str], chunk_method: Callable) -> dict[str, tuple[float, list[str]]]:
         """
         Perform sparse retrieval for each query.
 
@@ -43,80 +44,83 @@ class BM25Retriever(SparseRetriever):
         for a in aspects:
             a_text = a.get_new_description()
             # compute bm25 scores for a_text and dest_chunks
-            tokenized_a = a_text.split(" ") #TODO: modify tokenization method
+            tokenized_a = word_tokenize(a_text)
 
-            top_chunks = bm25.get_top_n(tokenized_a, corpus, n=num_chunks)
             # get the score for the top chunks
-            scores = bm25.get_scores(tokenized_a)
-            scores.sort(reversed=True)
+            score = bm25.get_scores(tokenized_a)
+            top_idx = chunk_method(score)
+            top_score = score[top_idx]
+            avg_score = np.sum(top_score) / top_score.shape[0]
 
-            avg_score = sum(scores[: num_chunks]) / num_chunks
+            # retrieve top chunks
+            chunks = np.array(dest_chunks) # [chunk_size]
+            top_chunks = chunks[top_idx].tolist()
 
             # store results
             dest_result[a_text] = (avg_score, top_chunks)
         return dest_result
     
 
-class SpladeRetriever(SparseRetriever):
-    """
-    Concrete SparseRetriever class.
-    """
+# class SpladeRetriever(SparseRetriever):
+#     """
+#     Concrete SparseRetriever class.
+#     """
 
-    def __init__(self, query_path: str, chunks_dir: str, output_path: str, num_chunks: int = 3, batchsize: int = 8):
-        super().__init__(query_path=query_path, output_path=output_path, chunks_dir=chunks_dir, num_chunks=num_chunks)
-        self.device = "cuda" if torch.cuda.is_available() else "cpu"
-        print(f"Using device: {self.device}")
-        self.model = model.Splade(
-            model=AutoModelForMaskedLM.from_pretrained("naver/splade_v2_max").to(self.device),
-            tokenizer=AutoTokenizer.from_pretrained("naver/splade_v2_max"),
-            device=self.device
-        )
-        self.batch_size = batchsize
+#     def __init__(self, query_path: str, chunks_dir: str, output_path: str, num_chunks: int = 3, batchsize: int = 8):
+#         super().__init__(query_path=query_path, output_path=output_path, chunks_dir=chunks_dir, num_chunks=num_chunks)
+#         self.device = "cuda" if torch.cuda.is_available() else "cpu"
+#         print(f"Using device: {self.device}")
+#         self.model = model.Splade(
+#             model=AutoModelForMaskedLM.from_pretrained("naver/splade_v2_max").to(self.device),
+#             tokenizer=AutoTokenizer.from_pretrained("naver/splade_v2_max"),
+#             device=self.device
+#         )
+#         self.batch_size = batchsize
     
-    def retrieval_for_dest(self, aspects: list[Aspect], dest_chunks: list[str], num_chunks: int = 3) -> dict[str, tuple[float, list[str]]]:
-        """
-        Perform sparse retrieval for each query.
+#     def retrieval_for_dest(self, aspects: list[Aspect], dest_chunks: list[str], num_chunks: int = 3) -> dict[str, tuple[float, list[str]]]:
+#         """
+#         Perform sparse retrieval for each query.
 
-        :param queries: list[str], list of query strings.
-        :param dests_chunks: dict[str, list[str]], dictionary of destination names to lists of associated text chunks.
-        :param percentile: float, percentile to determine the similarity threshold for filtering results.
-        :return: dict[str, dict[str, tuple[float, list[str]]]], structured results with scores and top matching chunks.
-        """
-        dest_result = dict()
-        aspects_text = [a.get_new_description() for a in aspects]
-        docs = [{"id": i, "chunks": chunk} for i, chunk in enumerate(dest_chunks)]
-        retriever = retrieve.SpladeRetriever(
-            key="id", # Key identifier of each document.
-            on="chunks", # Fields to search.
-            model=self.model # Splade retriever.
-        )
+#         :param queries: list[str], list of query strings.
+#         :param dests_chunks: dict[str, list[str]], dictionary of destination names to lists of associated text chunks.
+#         :param percentile: float, percentile to determine the similarity threshold for filtering results.
+#         :return: dict[str, dict[str, tuple[float, list[str]]]], structured results with scores and top matching chunks.
+#         """
+#         dest_result = dict()
+#         aspects_text = [a.get_new_description() for a in aspects]
+#         docs = [{"id": i, "chunks": chunk} for i, chunk in enumerate(dest_chunks)]
+#         retriever = retrieve.SpladeRetriever(
+#             key="id", # Key identifier of each document.
+#             on="chunks", # Fields to search.
+#             model=self.model # Splade retriever.
+#         )
 
-        retriever = retriever.add(
-            documents=docs,
-            batch_size=self.batch_size,
-            k_tokens=256, # Number of activated tokens.
-        )
+#         retriever = retriever.add(
+#             documents=docs,
+#             batch_size=self.batch_size,
+#             k_tokens=256, # Number of activated tokens.
+#         )
 
-        splade_results = retriever(
-            aspects_text, # Aspect
-            k_tokens=20, # Maximum number of activated tokens.
-            k = num_chunks, # Number of top-k documents to retrieve.
-            batch_size=self.batch_size
-        )
+#         splade_results = retriever(
+#             aspects_text, # Aspect
+#             k_tokens=20, # Maximum number of activated tokens.
+#             k = num_chunks, # Number of top-k documents to retrieve.
+#             batch_size=self.batch_size
+#         )
         
-        count = 0
-        for result in splade_results:
-            top_chunks = []
-            score =[]
-            for r in result:
-                top = next((doc["chunks"] for doc in docs if doc["id"] == r["id"]), None)
-                if top is not None:
-                    top_chunks.append(top)
-                    score.append(r["similarity"])
+#         count = 0
+#         for result in splade_results:
+#             top_chunks = []
+#             score =[]
+#             for r in result:
+#                 top = next((doc["chunks"] for doc in docs if doc["id"] == r["id"]), None)
+#                 if top is not None:
+#                     top_chunks.append(top)
+#                     score.append(r["similarity"])
                     
-            avg_score = sum(score) / len(score)
-            dest_result[aspects_text[count]] = (avg_score, top_chunks)
-            count += 1
-        return dest_result
+#             avg_score = sum(score) / len(score)
+#             dest_result[aspects_text[count]] = (avg_score, top_chunks)
+#             count += 1
+#         return dest_result
 
        
